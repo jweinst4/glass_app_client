795328df18d0b5641434d9078fb56f6a0d9a039d
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-07-01 23:45:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom'
+
+import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css';
+
+import './App.css';
+
+let baseURL = process.env.REACT_APP_BASEURL
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000'
+} else {
+  baseURL = 'https://shirt-api.herokuapp.com'
+}
+
+console.log('current base URL:', baseURL)
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+
+      }
+
+
+  render() {
+
+    return (
+
+  <p>Hi</p>
+
+       
+    );
+  }
+}
+
+export default App;

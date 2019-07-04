db2afccf5eccb45fe1c97877307e90587dcc07d7
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-07-04 03:01:45 -0400

    dropdown working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import 'materialize-css'; // It installs the JS asset only
 import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css'; 
 
 let baseURL = process.env.REACT_APP_BASEURL
 
@@ -19,14 +20,15 @@ class Navbar extends React.Component {
         this.state = {
        
         }
-     this.showItems = this.showItems.bind(this)
+
     }
 
+componentDidMount() {
+  // https://stackoverflow.com/questions/46207198/how-i-can-use-dropdown-from-materialize-css-in-react
+  let elems = document.querySelectorAll('.dropdown-trigger');
+  M.Dropdown.init(elems, {inDuration: 300, outDuration: 225});
 
-showItems() {
-    
 }
-    
 
     render() {
         return (
@@ -35,10 +37,9 @@ showItems() {
                 
 
                 <ul id="dropdown1" className="dropdown-content">
-  <li><a href="#!">one</a></li>
-  <li><a href="#!">two</a></li>
-  <li className="divider"></li>
-  <li><a href="#!">three</a></li>
+  <li><a href="/aboutUs/overview">Overview</a></li>
+  <li><a href="/aboutUs/management">Management</a></li>
+  <li><a href="/aboutUs/news">News</a></li>
 </ul>
 <nav>
   <div className="nav-wrapper navBar">
@@ -48,7 +49,7 @@ showItems() {
       <li><a href="#"><span className = 'navText'>ContactInfo</span></a></li>
       <li><a href="#"><span className = 'navText'>Resources</span></a></li>
       
-      <li><a className="dropdown-trigger" href="#" data-target="dropdown1" onClick={this.showItems}><span className = 'navText'>CompanyInfo</span><i className="material-icons right"><span className = 'navText'>arrow_drop_down</span></i></a></li>
+      <li><a className="dropdown-trigger" href="#" data-target="dropdown1" onClick={this.showItems}><span className = 'navText'>AboutUs</span><i className="material-icons right"><span className = 'navText'>arrow_drop_down</span></i></a></li>
     </ul>
   </div>
 </nav>

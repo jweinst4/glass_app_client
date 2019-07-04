b19133f9a77ae434b7f2a6e4bfc67c2baf17492d
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-07-04 02:05:25 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -21,9 +21,6 @@ import './App.css';
 
 require('dotenv').config()
 
-
-let baseURL = process.env.REACT_APP_BASEURL
-
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3000'
 } else {

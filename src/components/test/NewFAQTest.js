2a8a2a96448405c271bd2d67592421d423e7e8c2
jonathan weinstein <jonathanweinstein@jonathans-MBP.home>
2019-07-12 20:42:24 -0400

    new faq working

--- src/components/test/NewFAQTest.js
+++ src/components/test/NewFAQTest.js
@@ -40,6 +40,15 @@ class NewFAQTest extends React.Component {
         this.props.getFAQs()
         this.setState({question: ''})
         this.setState({answer: ''})
+        this.setState({answertwo: ''})
+        this.setState({answerthree: ''})
+        this.setState({answerfour: ''})
+        this.setState({answerfive: ''})
+        this.setState({answersix: ''})
+        this.setState({answerseven: ''})
+        this.setState({answereight: ''})
+        this.setState({answernine: ''})
+        this.setState({answerten: ''})
         this.toggleEdit()
     }
 
@@ -79,14 +88,22 @@ class NewFAQTest extends React.Component {
 
     handleFAQSubmit(event) {
         event.preventDefault()
-console.log(this.state.question)
-console.log(this.state.answer)
+
         if (this.props.fakeAuth.isAuthenticated) {
         fetch(baseURL + '/faqs', {
             method: 'POST',
             body: JSON.stringify({
                 question: this.state.question,
                 answer: this.state.answer,             
+                answertwo: this.state.answertwo,             
+                answerthree: this.state.answerthree,             
+                answerfour: this.state.answerfour,             
+                answerfive: this.state.answerfive,             
+                answersix: this.state.answersix,             
+                answerseven: this.state.answerseven,             
+                answereight: this.state.answereight,             
+                answernine: this.state.answernine,             
+                answerten: this.state.answerten,             
             }),
             headers: {
                 'Content-Type': 'application/json'
@@ -97,6 +114,15 @@ console.log(this.state.answer)
         this.setState({
             question: '',
             answer: '',
+            answertwo: '',
+            answerthree: '',
+            answerfour: '',
+            answerfive: '',
+            answersix: '',
+            answerseven: '',
+            answereight: '',
+            answernine: '',
+            answerten: '',
           })
         }
         else {
@@ -111,6 +137,15 @@ console.log(this.state.answer)
             faqs: copyFAQs,
             question: '',
             answer: '',
+            answertwo: '',
+            answerthree: '',
+            answerfour: '',
+            answerfive: '',
+            answersix: '',
+            answerseven: '',
+            answereight: '',
+            answernine: '',
+            answerten: '',
           })
         }
 
@@ -147,6 +182,69 @@ console.log(this.state.answer)
                 </div>   
                 </div>
 
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answertwo">answertwo</label>
+                <input className = 'col s6 m6 l6' type="text" id="answertwo" name="answertwo" onChange={this.handleFAQChange} value={this.state.answertwo}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answerthree">answerthree</label>
+                <input className = 'col s6 m6 l6' type="text" id="answerthree" name="answerthree" onChange={this.handleFAQChange} value={this.state.answerthree}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answerfour">answerfour</label>
+                <input className = 'col s6 m6 l6' type="text" id="answerfour" name="answerfour" onChange={this.handleFAQChange} value={this.state.answerfour}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answerfive">answerfive</label>
+                <input className = 'col s6 m6 l6' type="text" id="answerfive" name="answerfive" onChange={this.handleFAQChange} value={this.state.answerfive}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answersix">answersix</label>
+                <input className = 'col s6 m6 l6' type="text" id="answersix" name="answersix" onChange={this.handleFAQChange} value={this.state.answersix}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answerseven">answerseven</label>
+                <input className = 'col s6 m6 l6' type="text" id="answerseven" name="answerseven" onChange={this.handleFAQChange} value={this.state.answerseven}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answereight">answereight</label>
+                <input className = 'col s6 m6 l6' type="text" id="answereight" name="answereight" onChange={this.handleFAQChange} value={this.state.answereight}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answernine">answernine</label>
+                <input className = 'col s6 m6 l6' type="text" id="answernine" name="answernine" onChange={this.handleFAQChange} value={this.state.answernine}  />  
+                </div>   
+                </div>
+
+                <div className = 'form-inline'>
+            <div className = 'col s12 m12 l12 form-group'>
+            <label className = 'col s2 m2 l2' htmlFor="answerten">answerten</label>
+                <input className = 'col s6 m6 l6' type="text" id="answerten" name="answerten" onChange={this.handleFAQChange} value={this.state.answerten}  />  
+                </div>   
+                </div>
+
                 <div className = 'form-row'>
                 <input type="submit" value="Add a FAQ"/>
                 </div>       
@@ -163,6 +261,43 @@ console.log(this.state.answer)
                     <p>
                        Answer: {item.answer}
                     </p>
+
+                    <p>
+                       answertwo: {item.answertwo}
+                    </p>
+
+                    <p>
+                       answerthree: {item.answerthree}
+                    </p>
+
+                    <p>
+                       answerfour: {item.answerfour}
+                    </p>
+
+                    <p>
+                       answerfive: {item.answerfive}
+                    </p>
+
+                    <p>
+                       answersix: {item.answersix}
+                    </p>
+
+                    <p>
+                       answerseven: {item.answerseven}
+                    </p>
+
+                    <p>
+                       answereight: {item.answereight}
+                    </p>
+
+                    <p>
+                       answernine: {item.answernine}
+                    </p>
+
+                    <p>
+                       answerten: {item.answerten}
+                    </p>
+
                     <p onClick={() => { this.handleFAQDelete(item.id) }} >
                         <i className="small material-icons adminDelete">
                             delete
